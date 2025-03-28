2ac37ea2604ad8a303094a7dc2a21f0a6c01485f
Elias Peschke <eliaspeschke@googlemail.com>
2025-03-28 21:36:17 +0100

    ready for memoized draw functions

--- lib/LTest.hs
+++ lib/LTest.hs
@@ -1,5 +1,12 @@
+{-# LANGUAGE TypeOperators, TypeFamilies, FlexibleInstances #-}
+
 module LTest where
-import LSystem (DrawState)
+import LSystem (DrawState, Turtle (tuPosition, tuDirection, tuDirectionPerp, Turtle))
+import Control.Parallel.Strategies (parMap, rpar)
+import Linear
+import Control.Lens 
+import Data.MemoTrie
+import Control.Arrow (first)
 
 data TTree = TNode String [TTree] | TLeaf String
 
@@ -45,15 +52,59 @@ getBrackPart ('[' : xs) = let (_, a, b) = foldl cntBracks (1, "", "") xs in (a, 
 getBrackPart "" = ("", "")
 getBrackPart _  = error "getBrackPart got a String that does not start with [" 
 
---unparseTree :: TTree -> String
---unparseTree (TLeaf t) = t
---unparseTree (TNode t xs) = concat (t : map handle xs)
-  --          where
-    --        handle x = T.snoc (T.cons '[' $ unparseTree x) ']'
-    --
---type TDrawRules = DrawState -> Char -> DrawState
+type TDrawRules w = Char -> (DrawState, w) -> (DrawState, w)
 
---evalTTreeW :: (Monoid w) => DrawRulesW w -> DrawState -> LTree -> w
---evalTTreeW r st (TLeaf t) = let (_, res) = drawW r t st in res
---evalTTreeW r st (TNode t d) = let (newSt, res) = drawW r t st in
-                            ( res <> mconcat (parMap rpar (evalLTreeW r newSt) d))
+type DrawFunc w = DrawState -> String -> (DrawState, w)
+
+evalTTreeW :: (Monoid w) => DrawFunc w -> DrawState -> TTree -> w
+evalTTreeW f st (TLeaf t) = let (_, res) = f st t in res
+evalTTreeW f st (TNode t d) = let (newSt, res) = f st t in
+                            ( res <> mconcat (parMap rpar (evalTTreeW f newSt) d))
+
+mkDraw :: (Monoid w) => TDrawRules w -> DrawFunc w
+mkDraw rules = memo inner
+    where 
+    inner state = foldl (flip rules) (state, mempty) 
+
+debugRules :: TDrawRules String
+debugRules 'F' (tu, w) = ( [st { tuPosition = newpos }] 
+                          , w ++ show (newpos ^._x) ++ "," )
+    where
+    st = head tu 
+    pos = tuPosition  st
+    dir = tuDirection st
+   -- per = tuDirectionPerp st
+    newpos = pos + dir
+debugRules _ x = x
+
+enum' :: (HasTrie a) => (a -> a') -> (a :->: b) -> [(a', b)]
+enum' f = (fmap.first) f . enumerate
+
+instance HasTrie Float where
+  data Float :->: x = FloatTrie ((Integer, Int) :->: x)
+  trie f = FloatTrie $ trie (f . uncurry encodeFloat)
+  untrie (FloatTrie t) = untrie t . decodeFloat
+  enumerate (FloatTrie t) = enum' (uncurry encodeFloat) t
+
+instance (HasTrie a) => HasTrie (V3 a) where
+  data (V3 a) :->: x = V3Trie ([a] :->: x)
+  trie f = V3Trie $ trie (f . (\x -> V3 (head x) (x !! 1) (x !! 2)))
+  untrie (V3Trie t) = untrie t . (\(V3 a b c) -> [a,b,c])
+  enumerate (V3Trie t) = enum' (\x -> V3 (head x) (x !! 1) (x !! 2)) t
+
+
+turtle2List :: Turtle -> [V3 Float]
+turtle2List tu = [tuPosition tu, tuDirection tu, tuDirectionPerp tu]
+
+list2Turtle :: [V3 Float] -> Turtle
+list2Turtle xs =  Turtle {  tuPosition = head xs 
+                             , tuDirection = xs !! 1
+                             , tuDirectionPerp = xs !! 2 }  
+
+instance HasTrie Turtle where
+  data Turtle :->: x = DrawStateTrie ([V3 Float] :->: x)
+  trie f = DrawStateTrie $ trie (f. list2Turtle)
+  untrie (DrawStateTrie t) = untrie t . turtle2List 
+  enumerate (DrawStateTrie t) = enum' list2Turtle t
+
+
